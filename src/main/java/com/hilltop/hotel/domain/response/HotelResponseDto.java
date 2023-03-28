1e879e7e33d93e4cc4ed18db0249defe1d3d4728
bavanraj-swivel <bavanraj@swivelgroup.com.au>
2023-03-28 22:54:34 +0530

    Response change.

--- src/main/java/com/hilltop/hotel/domain/response/HotelResponseDto.java
+++ src/main/java/com/hilltop/hotel/domain/response/HotelResponseDto.java
@@ -1,5 +1,6 @@
 package com.hilltop.hotel.domain.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.hilltop.hotel.domain.entity.Hotel;
 import com.hilltop.hotel.domain.entity.Room;
 import lombok.Getter;
@@ -16,6 +17,7 @@ public class HotelResponseDto implements ResponseDto {
     private final String id;
     private final String name;
     private final String location;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<RoomResponseDto> rooms;
 
     public HotelResponseDto(Hotel hotel) {
